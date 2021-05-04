52f05ceaa6282bb0bd56bb7d76b0590a10cac424
Andrey-75 <83639094+Andrey-75@users.noreply.github.com>
2021-05-04 13:38:57 +0300

    Delete the_task4.py

--- the_task4.py
+++
@@ -1,8 +0,0 @@
-nun_name = int(input('Введите целое положителье число'))
-nun_max = nun_name % 10
-nun_name = nun_name // 10
-while nun_name > 0:
-    if nun_name % 10 > nun_max:
-        nun_max = nun_name % 10
-    nun_name = nun_name // 10
-print(nun_max)
